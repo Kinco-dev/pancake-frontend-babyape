1781675bf34a6234000f0bd18a5cea217e93d7a0
ColinK <c.kindt@synchrone.be>
2022-09-02 17:58:49 +0200

    update logo and color

--- packages/uikit/src/theme/colors.ts
+++ packages/uikit/src/theme/colors.ts
@@ -2,9 +2,9 @@ import { Colors } from "./types";
 
 export const baseColors = {
   failure: "#ED4B9E",
-  primary: "#1FC7D4",
-  primaryBright: "#53DEE9",
-  primaryDark: "#0098A1",
+  primary: "#fd04ec",
+  primaryBright: "#ff00fc",
+  primaryDark: "#dd0edb",
   secondary: "#7645D9",
   success: "#31D0AA",
   warning: "#fdda04",
